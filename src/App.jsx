d44fefc2d5cc3b2a6e044e62013950f718c8f2f4
David Ho <kietho.060193>
2024-12-01 21:48:50 +0700

    010 Nested Routes and Index Route

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,14 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<h1>LIST OF CITIES</h1>} />
+          <Route path="cities" element={<h1>LIST OF CITIES</h1>} />
+          <Route path="countries" element={<h1>LIST OF COUNTRIES</h1>} />
+          <Route path="form" element={<h1>FORM</h1>} />
+        </Route>
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import AppNav from './AppNav';
 import Footer from './Footer';
 import Logo from './Logo';
@@ -8,7 +9,7 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <h1>List of Cities</h1>
+      <Outlet />
       <Footer />
     </div>
   );
