570d90ad82031980e8096bf61d69a03ee2887b52
David Ho <kietho.060193>
2024-12-02 16:19:16 +0700

    013 Interacting With the Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,29 +1,40 @@
 import styles from './Map.module.css';
-import { useSearchParams } from 'react-router-dom';
-import { useState } from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from 'react-leaflet';
 import useCities from '../contexts/useCities';
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([16.4642982, 107.5875636]);
+
   const [searchParams] = useSearchParams();
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
+
+  useEffect(() => {
+    if (lat && lng) setMapPosition([lat, lng]);
+  }, [lat, lng]);
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
         className={styles.map}
         center={mapPosition}
-        zoom={13}
+        zoom={10}
         scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
         {cities.map((city) => (
           <Marker
             position={[city.position.lat, city.position.lng]}
@@ -32,9 +43,26 @@ function Map() {
             <Popup>{city.cityName}</Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => {
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
